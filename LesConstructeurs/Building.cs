632bf870cd798a088faad4f4b68ee6ea2fde95aa
AnSimonyan <anna.simona1976@gmail.com>
2024-06-18 17:30:15 +0300

    Добавьте файлы проекта.

---
+++ LesConstructeurs/Building.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesConstructeurs
+{
+    internal class Building
+    {
+        private int _numberOfFloors { get; set; }
+        private int _buildingSize { get; set; }
+        public int _height { get; set; } = 3;
+
+        public Building(int numberOfFloors, int buildingSize = 0)
+        {
+            _numberOfFloors = numberOfFloors;
+            if (buildingSize != 0)
+            {
+                _buildingSize = buildingSize;
+            }
+            else
+            {
+                _buildingSize = numberOfFloors * _height;
+            }
+        }
+
+        public int GetFloorMaxSize()
+        {
+            if (_numberOfFloors != 0)
+            {
+                return _buildingSize / _numberOfFloors;
+            }
+            else 
+            {
+                return 0;
+            }
+                
+        }
+
+        public int GetSize()
+        {
+            return _buildingSize;
+        }
+
+        public int SetHight(int hight)
+        {
+            return _height = hight;
+        }
+    }
+}
---
+++ LesConstructeurs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LesConstructeurs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Building myBuilding = new Building(3, 15);
+            
+            Console.WriteLine(myBuilding.GetFloorMaxSize());
+            Console.WriteLine(myBuilding.GetSize());
+
+        }
+    }
+}
